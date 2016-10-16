71998f034367a6cd3f2a7b0ed7910b23e8acaef2
Robin <robin.somlette@gmail.com>
2016-10-16 18:05:13 -0400

    first commit

---
+++ extandableView-test/MoveableView.swift
@@ -0,0 +1,21 @@
+//
+//  MoveableView.swift
+//  extandableView-test
+//
+//  Created by Robin Somlette on 16-Oct-2016.
+//  Copyright © 2016 Robin Somlette. All rights reserved.
+//
+
+import UIKit
+
+class MoveableView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
